0014dd3579eba54ddfa551b6084b97bc713d3d86
lderigoRealtec <lderigo@realtec.com.br>
2023-01-06 12:07:07 -0300

    Incompleto

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  SafeAreaView,
+} from 'react-native';
+
+import Home from './src'
+
+
+const App: () => Node = () => {
+
+  return (
+    <SafeAreaView>
+      <Home/>
+    </SafeAreaView>
+  );
+};
+
+export default App;
